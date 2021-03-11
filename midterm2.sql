3aa73be911e05624254df0d7ae24aa32b77b73a7
TestBasedRPG <37494710+TestBasedRPG@users.noreply.github.com>
2021-03-10 18:56:03 -0700

    Add files via upload

--- midterm2.sql
+++ midterm2.sql
@@ -1,7 +1,12 @@
 SELECT
-  *
+  invoice_line.invoice_id,
+  line_no,
+  sum(tax) OVER (PARTITION BY invoice_line.invoice_id) as sum_tax,
+  avg(tax) OVER (PARTITION BY invoice_line.invoice_id) as avg_tax,
+  zip_code
 FROM
-  address
-  INNER JOIN invoice_line
+  invoice_line
+  JOIN address on (address.invoice_id = invoice_line.invoice_id),
+  invoice
 ORDER BY
-  line_no;
+  line_no
